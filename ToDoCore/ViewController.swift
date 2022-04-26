11cf2dbf5a09927490a42157ee4577bbbcb718bb
Denis <chainsawfish@gmail.com>
2022-04-26 13:34:37 +0700

    Initial Commit

---
+++ ToDoCore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoCore
+//
+//  Created by Denis Borneman on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
